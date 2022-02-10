19f4c244cccb3c14000b5d8857bd6fa7f2ee2d28
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-10 15:38:38 +0200

    correcting paths

--- public/header.php
+++ public/header.php
@@ -3,6 +3,6 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-        <link href="style.css" rel="stylesheet" type="text/css">
+        <link href="./css/style.css" rel="stylesheet" type="text/css">
 
 	</head>
