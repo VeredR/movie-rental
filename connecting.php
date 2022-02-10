638438c4a788e78a6aa68f077129c4a1f10229c1
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-10 19:51:30 +0200

    for testing

---
+++ connecting.php
@@ -0,0 +1,51 @@
+<?php 
+require "./header.php";
+if(isset($errorMsg))
+{
+  foreach($errorMsg as $error)
+  {
+?>
+<div class = "alert alert-danger">
+  <strong><?php echo $error; ?></strong>
+</div>
+<?php
+  }
+} 
+if(isset($loginMsg))
+{
+?>
+<div class="alert alert-success">
+  <strong><?php echo $loginMsg; ?></strong>
+</div>
+<?php
+}
+?>
+<form method="post" class="form horizontal" action="./app/login.php">
+
+<div class="form-group">
+  <lable class="col-sm-3 control-label">Username or Email</lable>
+  <div class="col-sm-6">
+    <input type="text" name="text_username_email" class ="form-control" placeholder="Enter username or email"/>
+  </div>
+</div> 
+
+<div class="form-group">
+  <lable class="col-sm-3 control-label">Password</lable>
+  <div class="col-sm-6">
+    <input type="text" name="text_password" class ="form-control" placeholder="Enter Password"/>
+  </div>
+</div> 
+
+<div class="form-group">
+  <div class="col-sm-offset-3 col-sm-9 m-t-15">
+    <input type="submit" name="btn_login" class ="btn btn-success" value="Login">
+  </div>
+</div> 
+
+<div class="form-group">
+  <div class = "col-sm-offset-3 col-sm-9 m-t-15">
+    You don't have an account? register here: <a href="register.php"><p class ="text-info">Register Account</p></a>
+</div>
+</div>
+
+</form>
--- index.php
+++ index.php
@@ -1,51 +1,38 @@
+<center>
+<h2>
+<?php
+require_once "./app/user.php";
+require "./app/movies.php";
+
+/*session_start();
+if(!isset($_SESSION['user_login'])){
+    header("locaion: index.php");
+}
+
+$id = $_SESSION['user_login'];
+$user = get_user_by_id($id);
+
+if(isset($_SESSION['user_login'])){
+?>
+Welcome, 
 <?php 
-require "./header.php";
-if(isset($errorMsg))
-{
-  foreach($errorMsg as $error)
-  {
-?>
-<div class = "alert alert-danger">
-  <strong><?php echo $error; ?></strong>
-</div>
-<?php
-  }
-} 
-if(isset($loginMsg))
-{
-?>
-<div class="alert alert-success">
-  <strong><?php echo $loginMsg; ?></strong>
-</div>
-<?php
+echo $user;
+echo $user['username'];
 }
 ?>
-<form method="post" class="form horizontal" action="./app/login.php">
+</h2>
+*/
 
-<div class="form-group">
-  <lable class="col-sm-3 control-label">Username or Email</lable>
-  <div class="col-sm-6">
-    <input type="text" name="text_username_email" class ="form-control" placeholder="Enter username or email"/>
-  </div>
-</div> 
+<?php $movies =get_all_movies();
+  ?>
+<ul class="movies">
+   <?php foreach($movies as &$movie){?>
+    <a href= "./app/create_page.php?name=<?php echo $movie['name']; ?>">> <li><?php echo $movie['name']." release date:".$movie["release_date"];?></li></a>
+    <?php
+    } ?>
+</ul>
 
-<div class="form-group">
-  <lable class="col-sm-3 control-label">Password</lable>
-  <div class="col-sm-6">
-    <input type="text" name="text_password" class ="form-control" placeholder="Enter Password"/>
-  </div>
-</div> 
 
-<div class="form-group">
-  <div class="col-sm-offset-3 col-sm-9 m-t-15">
-    <input type="submit" name="btn_login" class ="btn btn-success" value="Login">
-  </div>
-</div> 
 
-<div class="form-group">
-  <div class = "col-sm-offset-3 col-sm-9 m-t-15">
-    You don't have an account? register here: <a href="register.php"><p class ="text-info">Register Account</p></a>
-</div>
-</div>
-
-</form>
+<a href="logout.php">Logout</a>
+</center>
