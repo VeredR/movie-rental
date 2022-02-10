23c86569d2ac26114177a6012a1eb51cddfa4bae
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-10 15:31:26 +0200

    adding header

---
+++ public/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
