0c11db06713bdc505cf898bd788d677adcde0713
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-10 15:46:31 +0200

    style

--- public/index.php
+++ public/index.php
@@ -23,32 +23,20 @@ if(isset($loginMsg))
 <form method="post" class="form horizontal">
 
 <div class="form-group login">
-  <lable class="col-sm-3 control-label">	<i class="fas fa-user"></i>Username or Email
+  <lable class="col-sm-3 control-label">	
+    <i class="fas fa-user"></i> Username or Email
 	</label>
-  <div class="col-sm-6">
     <input type="text" name="text_username_email" class ="form-control" placeholder="Enter username or email"/>
-  </div>
 
-
-
-  <lable class="col-sm-3 control-label"><i class="fas fa-lock"></i>Password
-
+  <lable class="col-sm-3 control-label">
+    <i class="fas fa-lock"></i> Password
   </lable>
-  <div class="col-sm-6">
     <input type="text" name="text_password" class ="form-control" placeholder="Enter Password"/>
-  </div>
  
-
-<div class="form-group login">
-  <div class="col-sm-offset-3 col-sm-9 m-t-15">
     <input type="submit" name="btn_login" class ="btn btn-success" value="Login">
-  </div>
 
+    <p class ="text-info"> You don't have an account? register here <a href="register.php">Register Account</p></a>
 
-<div class="form-group login">
-  <div class = "col-sm-offset-3 col-sm-9 m-t-15">
-    You don't have an account? register here <a href="register.php"><p class ="text-info">Register Account</p></a>
-</div>
 </div>
 
 </form>
