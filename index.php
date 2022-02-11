68a64fe6fa030761ac4f08e59b49f685a4319b56
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-11 16:13:57 +0200

    .

--- index.php
+++ index.php
@@ -1,22 +1,37 @@
 <?php
 require "./header.php";
-require_once "./app/movies.php";
-
-$all_movies = get_all_movies();
 ?>
+<center>
+<h2>
 
-<table>
+</h2>
+</center>
+
+<ul>
 <?php
-foreach($all_movies as $movie){
-            echo "<tr>" .
-                "<td>" . $movie["name"] . "</td>" .
-                "<td>" . $movie["score"] . "</td>" .
-                "<td>" . $movie["release_date"] . "</td>" .
-                "</tr>"; 
-}
+require "./app/movies.php";
+
+$movies = get_all_movies();
+echo "<div class= 'movies-list'><ul>"; 
+foreach($movies as &$movie){?>
+   <li class="movie">
+       <a href = "./app/create_page.php?name="<?php echo $movie['name']; ?>>
+          <div class="row">
+            <div class="col-md-9">
+                <h2><?php echo $movie['name'];?></h2>
+                <h3><?php echo $movie['score'];?></h3>
+                <div class= 'time'>
+                     <h3><?php echo $movie['release_date'];?></h3>
+                  </div>
+                </div>
+            </div>
+        </a>
+    </li>
+<?php  }
+echo "</ul></div>";
  ?>
- </table>
- 
 
 
 
+
+<a href="logout.php">Logout</a>
