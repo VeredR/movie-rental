be71b82dc78af006634ff83e5acc193ec9351634
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-11 14:55:28 +0200

    .

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require "./header.php";
 require "./app/movies.php";
 
 $movies = get_all_movies();
-print($movies);
+echo $movies;
 foreach($movies as &$movie){?>
    <li><?php echo $movie[0]['name']." release date:".$movie[0]["release_date"];?></li>;
     <?php  
