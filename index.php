6574ef6b6385cb04fb5dce91a65a460cc378512d
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-11 15:26:14 +0200

    .

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $movies = new movies();
 $all_movies =$movies->get_all_movies();
 
 foreach($all_movies as &$movie){?>
-   <a href ="./app/create_page.php?name=".<?php echo $movie['name'];?> ><li><?php echo $movie['name']." release date: ".$movie["release_date"];?></li></a>;
+  <li><?php echo $movie['name']." release date: ".$movie["release_date"];?></li>;
     <?php  
 }
  ?>
