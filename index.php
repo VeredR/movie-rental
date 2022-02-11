957bec63acc1cf4d3108ce064d1f5138d5940c24
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-11 14:39:38 +0200

    .

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require "./app/movies.php";
 $movies = get_all_movies();
 
 foreach($movies as &$movie){?>
-    <a href= "./app/create_page.php?name=<?php echo $movie['name']; ?>"><li> <?php echo $movie['name']." release date:".$movie["release_date"];?></li></a>
+    <!--a href= "./app/create_page.php?name=<?php //echo $movie['name']; ?>"--><li> <?php echo $movie['name']." release date:".$movie["release_date"];?></li><!--/a-->
     <?php
     } ?>
 </ul>
