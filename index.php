fe26be879ba7984af6ca8e51c96998f68afbf0ca
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-12 19:40:43 +0200

    .

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require "./app/movies.php";
 $movies = get_all_movies();
 if count($movies){
     echo "<div class='movies-list'><ul>"; 
-    foreach($movies as &$movie){
+    foreach($movies as $movie){
    ?>
    <li class="movie">
        <a href = "./app/create_page.php?name="<?php echo $movie['name']; ?>>
