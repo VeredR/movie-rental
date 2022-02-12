dbc8d84b7e7646701c6f84dbcaed563bc6f0d588
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-12 19:29:14 +0200

    .

--- header.php
+++ header.php
@@ -2,8 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link href="./css/style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-        
+        <link href="./css/style.css" rel="stylesheet" type="text/css">
 
 	</head>
