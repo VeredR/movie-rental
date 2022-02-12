df88c59dce556523180ed2760765b473f2044097
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-12 19:28:34 +0200

    .

--- index.php
+++ index.php
@@ -18,13 +18,13 @@ echo "<div class= 'movies-list'><ul>";
 foreach($movies as $movie){
    ?>
    <li class="movie">
-      <!-- <a href = "./app/create_page.php?name="<?php echo $mov['name']; ?>>-->
+      <!-- <a href = "./app/create_page.php?name="<?php //echo $movie['name']; ?>>-->
           <div class="row">
             <div class="col-md-9">
-                <h2><?php echo $mov['name'];?></h2>
-                <h3><?php echo $mov['score'];?></h3>
+                <h2><?php echo $movie['name'];?></h2>
+                <h3><?php echo $movie['score'];?></h3>
                 <div class= 'time'>
-                     <h3><?php echo $mov['release_date'];?></h3>
+                     <h3><?php echo $movie['release_date'];?></h3>
                 </div>
                 </div>
             </div>
