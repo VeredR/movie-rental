326a00579507212efc2f036126e9d30ce24834bf
Vered Rosengarten <vered.Ros@gmail.com>
2022-02-12 19:08:55 +0200

    .

--- index.php
+++ index.php
@@ -13,21 +13,23 @@ require "./app/movies.php";
 
 $movies = get_all_movies();
 echo "<div class= 'movies-list'><ul>"; 
-foreach($movies as &$movie){?>
+foreach($movies as $movie){
+    foreach ($movie as &$mov){?>
    <li class="movie">
-      <!-- <a href = "./app/create_page.php?name="<?php echo $movie['name']; ?>>-->
+      <!-- <a href = "./app/create_page.php?name="<?php echo $mov['name']; ?>>-->
           <div class="row">
             <div class="col-md-9">
-                <h2><?php echo $movie['name'];?></h2>
-                <h3><?php echo $movie['score'];?></h3>
+                <h2><?php echo $mov['name'];?></h2>
+                <h3><?php echo $mov['score'];?></h3>
                 <div class= 'time'>
-                     <h3><?php echo $movie['release_date'];?></h3>
+                     <h3><?php echo $mov['release_date'];?></h3>
                 </div>
                 </div>
             </div>
       <!--  </a> -->
     </li>
 <?php  }
+}
 echo "</ul></div>";
  ?>
 
